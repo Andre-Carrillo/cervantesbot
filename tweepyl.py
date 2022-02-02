3009ecfc5843d469f57ce38528389303e5f9c181
andre-carrillo <carrillomonteroandre@gmail.com>
2022-02-01 21:56:08 -0500

    added something to fix it, hopefully

--- tweepyl.py
+++ tweepyl.py
@@ -9,7 +9,7 @@ load_dotenv()
 auth = tweepy.OAuthHandler(os.getenv("APIKEY"), os.getenv("APIKEY_SECRET"))
 auth.set_access_token(os.getenv("ATOKEN"),os.getenv("ATOKEN_SECRET"))
 
-api=tweepy.API(auth)
+api=tweepy.API(auth, wait_on_rate_limit=True)
 
 bookpath="./libro.txt"
 file = open(bookpath, "r", encoding="utf-8").read()
