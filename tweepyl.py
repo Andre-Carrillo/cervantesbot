f6b3ef93b397cbca9c862570dec37477e75a6ca7
andre-carrillo <carrillomonteroandre@gmail.com>
2022-02-01 19:16:42 -0500

    update

---
+++ tweepyl.py
@@ -0,0 +1,62 @@
+import tweepy
+import random
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+auth = tweepy.OAuthHandler(os.getenv("APIKEY"), os.getenv("APIKEY_SECRET"))
+auth.set_access_token(os.getenv("ATOKEN"),os.getenv("ATOKEN_SECRET"))
+
+api=tweepy.API(auth)
+
+bookpath="./libro.txt"
+f = open(bookpath, "r", encoding="utf-8").read()
+
+def tweet_quote(index=0, len_of_quote=200, id=None):
+    # f = open(bookpath, "r", encoding="utf-8").read()
+    if not index:
+        index=int(random.random()*(len(f)-len_of_quote))
+    quote=f[index:index+len_of_quote]
+    print(quote)
+    try:
+        api.update_status(quote, in_reply_to_status_id=id)
+    except:
+        print("Could not tweet")
+
+def countword(word):
+    # f = open(bookpath, "r", encoding="utf-8").read()
+    capital_letter_indexes=[index for index, value in enumerate(f) if value == word[0]]
+    word_indexes=[]
+    for index in capital_letter_indexes:
+        for i, lettre in enumerate(word):
+            if not f[index+i]==word[i]:
+                break
+        else:
+            word_indexes.append(index)
+    return(len(word_indexes))
+
+
+def mainloop():
+    i=0
+    lowest_id=1
+    print("Starting main loop...")
+    while True:
+        mentions=api.mentions_timeline(since_id=lowest_id)
+        print(f"Extracted {len(mentions)} mentions")
+        for mention in mentions:
+            raw_command=mention.text[15:]
+            command=raw_command.strip().split(" ")
+            if command[0]=="cita":
+                tweet_quote(index=int(command[1][8:]), id=mention.id)
+            elif command[0]=="contar":
+                api.update_status(f"{command[1]} se repite {countword(command[1][8:])} veces", in_reply_to_status_id=mention.id)
+            lowest_id=mention.id
+
+        if i%720==0:
+            dot_indexes=[index for index, value in enumerate(f) if value == "."]
+            tweet_quote(index=random.choice(dot_indexes))
+        i+=1
+        time.sleep(10)
+mainloop()
