4ca5cb0681b3660d5cb4c33a796d3d8a3ba7ba54
andre-carrillo <carrillomonteroandre@gmail.com>
2022-02-06 12:12:13 -0500

    added 3 new features

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from PIL import Image, ImageDraw
 import textwrap
 
-def quoteimage(book, rn=0): 
+def quoteimage(book, rn=0, reverse=False): 
     dot_indexes=[index for index, value in enumerate(book) if value == "."]
     index , end= dot_indexes[rn-1:rn+1]
     quote=book[index+1: end+1]
+    if reverse:
+        quote = quote[::-1]
     quote = textwrap.fill(quote, 40)
     nlines=quote.count("\n")
     print(nlines)
--- tweepyl.py
+++ tweepyl.py
@@ -3,8 +3,16 @@ import random
 import time
 import os
 from dotenv import load_dotenv
+import matplotlib.pyplot as plt
 
 from test import quoteimage
+
+## TODOS
+#Wrong request answer //done
+#histograma de frecuencia de una sílaba//done
+#quotes al revés//done
+#hacerlo con plotly
+#si hago videos, que lo narre con voz de loquendo
 
 load_dotenv()
 
@@ -16,16 +24,28 @@ api=tweepy.API(auth, wait_on_rate_limit=True)
 bookpath="./libro.txt"
 file = open(bookpath, "r", encoding="utf-8").read()
 
-def tweet_quote(index=0, len_of_quote=200, id=None):
+def tweet_quote(index=0, len_of_quote=200, id=None, reverse=False):
     if not index:
         index=int(random.random()*(len(file)-len_of_quote))
     quote=file[index:index+len_of_quote]
     try:
         # api.update_status_(quote, in_reply_to_status_id=id)
-        quoteimage(file, index).save("image.png")
+        quoteimage(file, index, reverse=reverse).save("image.png")
         api.update_status_with_media("",filename="image.png", in_reply_to_status_id=id)
     except Exception as e:
         print(f"[{time.localtime()[3]}:{time.localtime()[4]}:{time.localtime()[5]}-{time.localtime()[1:3]}]"+f"Could not tweet: {e}"+"\n")
+
+def sylhist(syl, bins=20):
+    capital_letter_indexes=[index for index, value in enumerate(file) if value == syl[0]]
+    word_indexes=[]
+    for index in capital_letter_indexes:
+        for i, _ in enumerate(syl):
+            if not file[index+i]==syl[i]:
+                break
+        else:
+            word_indexes.append(index)
+    plt.hist(word_indexes, bins=bins)
+    plt.savefig("./plot.png")
 
 def countword(word):
     capital_letter_indexes=[index for index, value in enumerate(file) if value == word[0]]
@@ -53,17 +73,28 @@ def mainloop(hours):
             for mention in mentions:
                 raw_command=mention.text[15:]
                 command=raw_command.strip().split(" ")
-                if command[0]=="cita":
-                    tweet_quote(index=int(command[1][8:]), id=mention.id)
-                elif command[0]=="contar":
-                    api.update_status(f"{command[1][8:]} se repite {countword(command[1][8:])} veces", in_reply_to_status_id=mention.id)
+                try:
+                    if command[0]=="cita":
+                        if command[2]=="reverso":
+                            tweet_quote(index=int(command[1][8:]), id=mention.id, reverse=True)
+                        else:
+                            tweet_quote(index=int(command[1][8:]), id=mention.id)
+                    elif command[0]=="contar":
+                        api.update_status(f"{command[1][8:]} se repite {countword(command[1][8:])} veces.", in_reply_to_status_id=mention.id)
+                    elif command[0]=="histograma":
+                        sylhist(command[1][8:])
+                        api.update_status_with_media("",filename="plot.png", in_reply_to_status_id=mention.id)                    
+                    else:
+                        api.update_status(f"Formato incorrecto.\nUsa los comandos 'cita' o 'contar' seguidos de 'empieza' o 'palabra' seguido de tu pedido.", in_reply_to_status_id=mention.id)
+                except:
+                    api.update_status(f"Se produjo un error. Revisa si el formato está escrito correctamente.", in_reply_to_status_id=mention.id)
+
+
                 log.write(f"[{time.localtime()[3]}:{time.localtime()[4]}:{time.localtime()[5]}-{time.localtime()[1:3]}]"+f"Command: '{command}' answered. Mention{mention.id}"+"\n")
                 with open("lastid.txt", "w", encoding="utf-8") as fi:
                     fi.write(str(mention.id))
                     log.write(f"[{time.localtime()[3]}:{time.localtime()[4]}:{time.localtime()[5]}-{time.localtime()[1:3]}]"+f"Changed lastid to {mention.id}"+"\n")
                 fi.close()
-                
-
         if (i+1)%(360*hours)==0:
             tweet_quote(index=random.choice(dot_indexes)+1)
             
