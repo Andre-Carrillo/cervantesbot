fcd150ee4e7de493a76e375bb01612bbe7b1dc47
andre-carrillo <carrillomonteroandre@gmail.com>
2022-02-10 20:00:04 -0500

    it was in this branch xd

--- periodic.py
+++ periodic.py
@@ -10,10 +10,10 @@ auth.set_access_token(os.getenv("ATOKEN"),os.getenv("ATOKEN_SECRET"))
 
 api=tweepy.API(auth, wait_on_rate_limit=True)
 
-bookpath="./libro.txt"
+bookpath="./textfiles/libro.txt"
 file = open(bookpath, "r", encoding="utf-8").read()
 dot_indexes=[index for index, value in enumerate(file) if value == "."]
 index=random.choice(dot_indexes)
-quoteimage(file, index).save("image.png")
+quoteimage(file, index).save("./images/image.png")
 
-api.update_status_with_media("", filename="image.png")
+api.update_status_with_media("", filename=".7images/image.png")
